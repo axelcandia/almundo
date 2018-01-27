f8b147f2f30bc4d866a718fd5d74047435f7463d
acandia <acandia@DESKTOP-7HU91UI>
2018-01-27 14:14:07 -0300

    Working with angularjs

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var express = require('express')
 
 var app = express()
-app.set('views', __dirname)
+app.use(express.static(__dirname + '/public'));
 // use whatever templating system(s) you like
 app.set('view engine', 'pug')
  
-// Load the routes ("controllers" -ish) 
-app.use('/api', require('./app/api/customers/router')) 
-// Repeat the above line for additional model areas ("deals", "vehicles", etc)
 
-// FINALLY, use any error handlers
-app.use(require('./app/api/errors/not-found'))
+// Load the routes ("controllers" -ish) 
+app.use('/api', require('./app/customers/router'))  
+
+
+
+//This is for the frontend
+app.get('*', function(req, res) {
+    res.sendfile('./public/views/index.html'); // load our public/index.html file
+});
 
 // Export the app instance for unit testing via supertest
 module.exports = app
