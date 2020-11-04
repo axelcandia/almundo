32463a18f2aff3c7f7001b214c4d8bb83361332a
Axl <axelcandia2609@gmail.com>
2020-11-04 10:15:54 -0300

    Update test.js

--- test.js
+++ test.js
@@ -2,3 +2,14 @@ function initMap(){
 var input = document.getElementById('checkout_shipping_address_address1');
     var autocomplete = new google.maps.places.Autocomplete(input);
 }
+
+
+function require(url) {
+    var e = document.createElement("script");
+    e.src = url;
+    e.type = "text/javascript";
+    e.addEventListener('load', initMap);
+    document.getElementsByTagName("head")[0].appendChild(e);
+}
+
+require("https://maps.googleapis.com/maps/api/js?key=AIzaSyAybfSVih3NyMkHZaVIVWghNupx0dXG7pU&libraries=places&callback=initMap");
